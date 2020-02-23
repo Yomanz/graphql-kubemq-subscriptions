5e20e66de62599e6fd52de5100099bfc61f71050
Daave <daave@widgetbot.io>
2020-02-23 22:20:08 +0000

    Fix unsub issues

--- src/pubsub.ts
+++ src/pubsub.ts
@@ -1,4 +1,4 @@
-import { PubSubEngine } from 'graphql-subscriptions';
+import {PubSubEngine} from 'graphql-subscriptions';
 import {Event, EventReceive, Publisher, PubSubSettings, Subscriber} from "kubets/compiled";
 import {PubSubAsyncIterator} from "./iterator";
 import {TextDecoder} from "util";
@@ -20,7 +20,8 @@ export class KubeMQPubSub implements PubSubEngine {
 	private readonly subscriber: Subscriber;
 	private readonly publisher: Publisher;
 
-	private subscriptions: { [message: string]: Function[] } = {};
+	private subscriptionMap: {[subId: number]: [string, Function]} = {};
+	private subsRefsMap: {[trigger: string]: number[]} = {};
 	private currentSubscriptionId: number = 0;
 
 	constructor(settings: KubeMQPubSubOptions) {
@@ -45,8 +46,8 @@ export class KubeMQPubSub implements PubSubEngine {
 		const body = event.getBody();
 		if (!message || !body) return; // Not from PubSub or invalid body.
 
-		const subscriptionCBs = this.subscriptions[message];
-		if (!subscriptionCBs) return; // No subs for this event.
+		const subscribers = this.subsRefsMap[message];
+		if (!subscribers) return; // No subs for this event.
 
 		let parsedBody;
 		try {
@@ -55,7 +56,10 @@ export class KubeMQPubSub implements PubSubEngine {
 			parsedBody = body;
 		}
 
-		for (const cb of subscriptionCBs) cb(parsedBody)
+		for (const subId of subscribers) {
+			const [, cb] = this.subscriptionMap[subId];
+			cb(parsedBody);
+		}
 	}
 
 	private onError(e: Error) {
@@ -77,15 +81,34 @@ export class KubeMQPubSub implements PubSubEngine {
 	async subscribe(trigger: string, onMessage: Function, options: Object = {}): Promise<number> {
 		const triggerName = this.triggerTransform(trigger, options);
 		const id = this.currentSubscriptionId += 1;
+		this.subscriptionMap[id] = [triggerName, onMessage];
+		let refs = this.subsRefsMap[triggerName];
 
-		if (!this.subscriptions[triggerName]) this.subscriptions[triggerName] = [];
-		this.subscriptions[triggerName].push(onMessage);
-
-		return id;
+		if (refs && refs.length > 0) {
+			this.subsRefsMap[triggerName] = [...refs, id];
+			return id;
+		} else {
+			this.subsRefsMap[triggerName] = [
+				...(this.subsRefsMap[triggerName] || []),
+				id,
+			];
+			return id;
+		}
 	}
 
 	unsubscribe(subId: number): any {
-		// const [triggerName, onMessage] = this.subscriptions[subId];
-		// delete this.subscriptions[subId];
+		const [triggerName] = this.subscriptionMap[subId] || [];
+		const refs = this.subsRefsMap[triggerName];
+
+		if (!refs) throw new Error(`There is no subscription of id: ${subId}`);
+
+		if (refs.length === 1) {
+			delete this.subsRefsMap[triggerName]
+		} else {
+			const index = refs.indexOf(subId);
+			this.subsRefsMap[triggerName] =
+				index === -1 ? refs : [...refs.slice(0, index), ...refs.slice(index + 1)];
+		}
+		delete this.subscriptionMap[subId]
 	}
 }
